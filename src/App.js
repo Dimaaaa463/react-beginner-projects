ea7561b975ab07f339683caaca9e4f39f7d086e2
Dmitriy463 <prilepskiids@gmail.com>
2022-12-01 00:12:45 +0700

    APP quiz half done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './index.scss';
 
 const questions = [
@@ -32,26 +33,36 @@ function Result() {
   );
 }
 
-function Game() {
+function Game( step, question, onClickVatiant ) {
+  const percentage = Math.round(step / questions.length * 100)
+
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{questions.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {
+        question.variants.map((text, index ) => <li onClick={() => onClickVatiant(index)} key={text}>{text}</li> 
+        )}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, setStep] = React.useState(0);
+  const question = questions[step];
+
+  const onClickVatiant = (index) => {
+    console.log(step, index)
+    setStep(step + 1)
+  }
+
   return (
     <div className="App">
-      <Game />
+      <Game step = {step} question={question} onClickVatiant={onClickVatiant}/>
       {/* <Result /> */}
     </div>
   );
